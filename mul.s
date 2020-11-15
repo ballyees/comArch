c9c5ad07b22ec1e4c06e40be7549d8ed2945a644
ballyees <shouldertip@gmail.com>
2020-11-15 17:16:37 +0700

    v1

---
+++ mul.s
@@ -0,0 +1,23 @@
+ lw 0 2 mcand
+ lw 0 3 mpiler
+ lw 0 4 pos
+ lw 0 5 and
+ nand 3 5 5
+ nand 5 5 5
+ beq 0 5 3
+ nand 3 3 3
+ add 3 4 3
+mul add 1 2 1
+ add 6 4 6
+ beq 6 3 2
+ beq 0 0 -3
+ beq 0 5 3
+ nand 1 1 1
+ add 1 4 1
+ noop
+ halt
+addrM .fill mul
+mcand .fill -5
+mpiler .fill -5
+and .fill -32768
+pos .fill 1
